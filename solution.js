f7532ab70ecb2379b8a40c1a7bdd82d9898ef37b
Naveed Usmani <naveedhd@gmail.com>
2016-04-13 17:57:05 +0900

    [Challenge 8]: Count

--- solution.js
+++ solution.js
@@ -1,8 +1,7 @@
 var mongo = require('mongodb').MongoClient
 
-var url = 'mongodb://localhost:27017/' +  process.argv[2];
-var collection = process.argv[3];
-var id = process.argv[4];
+var url = "mongodb://localhost:27017/learnyoumongo";
+var age = parseInt(process.argv[2], 10);
 
 // connect to specified url of database
 mongo.connect(url, function(err, db) {
@@ -10,13 +9,14 @@ mongo.connect(url, function(err, db) {
   if (err) { return console.dir(err); }
 
   // if connected, next try to find the collection
-  db.collection(collection, function (err, collection) {
+  db.collection('parrots', function (err, collection) {
     // if collection not found: exit
     if (err) { return console.dir(err); }
 
     // next update the document
-    collection.remove({ _id: id }, function(err, data) {
+    collection.count({ age: {$gt: age} }, function(err, count) {
       if (err) { return console.dir(err); }
+      console.log(count);
       db.close();
     });
   });
